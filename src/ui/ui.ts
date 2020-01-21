0fff60a7d8c8dafd2343b6c5b24e7adf256fa673
Jesse Anttila J J <35033029+jesseanttila-cai@users.noreply.github.com>
2020-01-21 16:30:00 +0200

    Hide UI action command, refactor & comment

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -7,33 +7,35 @@ export default class UI {
     constructor() {
         this.treeDP = new TmcTDP();
         const tree = vscode.window.createTreeView("tmcView", {treeDataProvider: this.treeDP});
-        this.treeDP.registerAction("login", true);
-        this.treeDP.registerAction("logout", false);
+        this.treeDP.registerAction("login", () => {
+            this.treeDP.setVisibility("login", false);
+            this.treeDP.setVisibility("logout", true);
+        }, true);
+        this.treeDP.registerAction("logout", () => {
+            this.treeDP.setVisibility("logout", false);
+            this.treeDP.setVisibility("login", true);
+        }, false);
     }
 
     public createUiActionHandler() {
-        return (label: string) => {
-            if (label === "login") {
-                this.treeDP.setVisibility("logout", true);
-                this.treeDP.setVisibility("login", false);
-            } else if (label === "logout") {
-                this.treeDP.setVisibility("logout", false);
-                this.treeDP.setVisibility("login", true);
-            }
-            vscode.window.showInformationMessage("Activated " + label);
+        return (onClick: () => void) => {
+            onClick();
         };
     }
 }
 
 class TmcTDP implements vscode.TreeDataProvider<TMCAction> {
 
+    // TreeView items are stored in this map
     public actions: Map<string, {action: TMCAction, visible: boolean}> = new Map();
 
-    public uonDidChangeTreeData: vscode.EventEmitter<TMCAction | undefined> =
+    // In order for VSCode to update the tree view list, an onDidChangeTreeData event must fire
+    // This emitter makes that possible to achieve (see refresh())
+    public refreshEventEmitter: vscode.EventEmitter<TMCAction | undefined> =
         new vscode.EventEmitter<TMCAction | undefined>();
+    public readonly onDidChangeTreeData: vscode.Event<TMCAction | undefined> = this.refreshEventEmitter.event;
 
-    public readonly onDidChangeTreeData: vscode.Event<TMCAction | undefined> = this.uonDidChangeTreeData.event;
-
+    // VSCode uses this to populate the TreeView, undefined -> return root items
     public getChildren(element?: TMCAction | undefined): TMCAction[] {
         if (element === undefined) {
             const actionList: TMCAction[] = [];
@@ -47,37 +49,36 @@ class TmcTDP implements vscode.TreeDataProvider<TMCAction> {
         return [];
     }
 
+    // Mandatory, identity function
     public getTreeItem(element: TMCAction) {
         return element;
     }
 
-    public registerAction(label: string, visible: boolean) {
+    // Adds an item using an unique label to the TreeView, visibility argument determines visibility
+    public registerAction(label: string, onClick: () => void, visible: boolean) {
         if (this.actions.get(label) !== undefined) {
             return;
         }
-
-        if (visible === undefined) {
-            visible = false;
-        }
         this.actions.set(label, {action: new TMCAction(label,
-             {command: "tmcView.activateEntry", title: "", arguments: [label]}), visible});
+             {command: "tmcView.activateEntry", title: "", arguments: [onClick]}), visible});
         this.refresh();
     }
 
+    // Modifies the visibility of a TreeView item by label, refreshing the TreeView if necessary.
     public setVisibility(label: string, visible: boolean) {
-        for (const action of this.actions) {
-            if (action[0] === label) {
-                if (action[1].visible !== visible) {
-                    action[1].visible = visible;
-                    this.refresh();
-                }
-                return;
+        const action = this.actions.get(label);
+
+        if (action) {
+            if (action.visible !== visible) {
+                action.visible = visible;
+                this.refresh();
             }
         }
     }
 
+    // Emits an event to trigger a TreeView refresh
     private refresh(): void {
-        this.uonDidChangeTreeData.fire();
+        this.refreshEventEmitter.fire();
     }
 
 }
