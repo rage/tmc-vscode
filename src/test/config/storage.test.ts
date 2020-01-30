dbee34f2666acb199fe067d71cada520b0993eac
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2020-01-30 17:04:10 +0200

    Tweaked storage tests

--- src/test/config/storage.test.ts
+++ src/test/config/storage.test.ts
@@ -7,6 +7,11 @@ import Storage from "../../config/storage";
 
 suite("Storage tests", () => {
 
+    // Same as hardcoded in storage. We also "magically" know them here to protect against data loss between releases
+    const AUTHENTICATION_TOKEN_KEY = "token";
+    const COURSE_KEY = "course";
+    const ORGANIZATION_KEY = "organization";
+
     function createMocks() {
         const mockMemento = TypeMoq.Mock.ofType<vscode.Memento>();
         const mockContext = TypeMoq.Mock.ofType<vscode.ExtensionContext>();
@@ -22,13 +27,13 @@ suite("Storage tests", () => {
         mockMemento
             .verify((x) => x.update(TypeMoq.It.isAnyString(), TypeMoq.It.isValue("test")), TypeMoq.Times.never());
         storage.updateOrganizationSlug("test");
-        mockMemento
-            .verify((x) => x.update(TypeMoq.It.isAnyString(), TypeMoq.It.isValue("test")), TypeMoq.Times.once());
+        mockMemento.verify((x) =>
+            x.update(TypeMoq.It.isValue(ORGANIZATION_KEY), TypeMoq.It.isValue("test")), TypeMoq.Times.once());
 
         // getting
         mockMemento.verify((x) => x.get(TypeMoq.It.isAnyString()), TypeMoq.Times.never());
         storage.getOrganizationSlug();
-        mockMemento.verify((x) => x.get(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+        mockMemento.verify((x) => x.get(TypeMoq.It.isValue(ORGANIZATION_KEY)), TypeMoq.Times.once());
     });
 
     test("Course id is stored correctly", () => {
@@ -40,12 +45,12 @@ suite("Storage tests", () => {
             .verify((x) => x.update(TypeMoq.It.isAnyString(), TypeMoq.It.isValue(1337)), TypeMoq.Times.never());
         storage.updateCourseId(1337);
         mockMemento
-            .verify((x) => x.update(TypeMoq.It.isAnyString(), TypeMoq.It.isValue(1337)), TypeMoq.Times.once());
+            .verify((x) => x.update(TypeMoq.It.isValue(COURSE_KEY), TypeMoq.It.isValue(1337)), TypeMoq.Times.once());
 
         // getting
         mockMemento.verify((x) => x.get(TypeMoq.It.isAnyString()), TypeMoq.Times.never());
         storage.getCourseId();
-        mockMemento.verify((x) => x.get(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+        mockMemento.verify((x) => x.get(TypeMoq.It.isValue(COURSE_KEY)), TypeMoq.Times.once());
     });
 
     test("Authentication token data is stored correctly", () => {
@@ -58,13 +63,13 @@ suite("Storage tests", () => {
         mockMemento.verify((x) => x.update(TypeMoq.It.isAnyString(),
                                            TypeMoq.It.isValue(tokenData)), TypeMoq.Times.never());
         storage.updateAuthenticationToken(tokenData);
-        mockMemento.verify((x) => x.update(TypeMoq.It.isAnyString(),
+        mockMemento.verify((x) => x.update(TypeMoq.It.isValue(AUTHENTICATION_TOKEN_KEY),
                                            TypeMoq.It.isValue(tokenData)), TypeMoq.Times.once());
 
         // getting
         mockMemento.verify((x) => x.get(TypeMoq.It.isAnyString()), TypeMoq.Times.never());
         storage.getAuthenticationToken();
-        mockMemento.verify((x) => x.get(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+        mockMemento.verify((x) => x.get(TypeMoq.It.isValue(AUTHENTICATION_TOKEN_KEY)), TypeMoq.Times.once());
     });
 
 });
