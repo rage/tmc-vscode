bf5ed90e602d63da3928262f5b837f8f46dfcb8f
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-02-20 10:13:23 +0200

    Fix bug

--- src/ui/templateEngine.ts
+++ src/ui/templateEngine.ts
@@ -88,26 +88,26 @@ export default class TemplateEngine {
             let percentDone = 0;
             if (status === "created") {
                 percentDone = 30;
-                return `<div class="progress py-1">
+                return `<div class="progress">
                             <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${percentDone}" aria-valuemin="0" aria-valuemax="100" style="width: ${percentDone}%"></div>
                         </div>
                         <div>&#10004; Submission received. Waiting for it to be processed.</div>`;
             } else if (status === "sending_to_sandbox") {
                 percentDone = 45;
-                return `<div class="progress py-1">
+                return `<div class="progress">
                             <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${percentDone}" aria-valuemin="0" aria-valuemax="100" style="width: ${percentDone}%"></div>
                         </div>
                         <div>&#10004; Submission received. Waiting for it to be processed.</div><div>Submission queued for processing.</div>`;
             } else if (status === "processing_on_sandbox") {
                 percentDone = 75;
-                return `<div class="progress py-1">
+                return `<div class="progress">
                             <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${percentDone}" aria-valuemin="0" aria-valuemax="100" style="width: ${percentDone}%"></div>
                         </div>
                         <div>&#10004; Submission received. Waiting for it to be processed.</div>
                         <div>&#10004; Submission in queue for processing.</div>
                         <div>Testing submission.</div>`;
             } else {
-                return `<div class="progress py-1">
+                return `<div class="progress">
                             <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${percentDone}" aria-valuemin="0" aria-valuemax="100" style="width: ${percentDone}%"></div>
                         </div>
                         <div>Submission sent to server.</div>`;
