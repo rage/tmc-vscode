fd6f0fb8fe51fb9ead8912b93a518f0ca904931b
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2020-03-05 12:46:24 +0200

    Separate Webpack & TypeScript config for dev/prod

---
+++ webpack.dev.js
@@ -0,0 +1,31 @@
+//@ts-check
+
+'use strict';
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+/**@type {import('webpack').Configuration}*/
+const devConfig = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            compiler: 'ttypescript',
+                            configFile: 'tsconfig.json'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+module.exports = merge(common, devConfig);
