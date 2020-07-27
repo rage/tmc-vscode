bf00d923f337f69b99a58ae3a1cc095298b608fd
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-07-27 12:08:54 +0300

    Show missing API exercises, deadline passed

--- src/utils/exerciseDecorator.ts
+++ src/utils/exerciseDecorator.ts
@@ -17,24 +17,47 @@ export class ExerciseDecorator implements vscode.DecorationProvider {
     > = new vscode.EventEmitter<vscode.Uri | vscode.Uri[]>().event;
 
     provideDecoration(uri: vscode.Uri): vscode.ProviderResult<vscode.Decoration> {
-        const exercise = this.workspaceManager.getExerciseDataByPath(uri.fsPath);
-        if (exercise.err) {
+        const exerciseAsPath = this.workspaceManager.getExerciseDataByPath(uri.fsPath);
+        if (exerciseAsPath.err) {
             return;
         }
+        const localExercise = exerciseAsPath.val;
+
+        const deadlinePassed = localExercise.deadline
+            ? Date.now() > Date.parse(localExercise.deadline)
+            : undefined;
+
         const workspace = vscode.workspace.name;
         if (!workspace) {
             return;
         }
-        const passed = this.userData
-            .getCourseByName(workspace.split(" ")[0])
-            .exercises.find((ex) => ex.id === exercise.val.id)?.passed;
-        if (passed) {
+
+        const course = this.userData.getCourseByName(workspace.split(" ")[0]);
+        const exercise = course.exercises.find((ex) => ex.id === localExercise.id);
+        if (!exercise) {
             return {
-                letter: "C",
-                title: "Completed!",
-                color: new vscode.ThemeColor("gitDecoration.addedResourceForeground"),
+                letter: "ⓘ",
+                title:
+                    "Exercise not found in course. " +
+                    "This could be an old exercise that has been renamed or removed from course.",
+                color: new vscode.ThemeColor("gitDecoration.ignoredResourceForeground"),
+                priority: 2,
+            };
+        }
+
+        if (exercise.passed) {
+            return {
+                letter: "✓",
+                title: "Exercise completed!",
                 priority: 1,
-                bubble: false,
+            };
+        }
+
+        if (deadlinePassed) {
+            return {
+                letter: "✗",
+                title: "Deadline exceeded.",
+                priority: 1,
             };
         }
     }
