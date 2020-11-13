9eb3bcd65b1076a524bf4136f349f5692ccbfbb0
Jori Lampi <jori.lampi@helsinki.fi>
2020-11-13 13:59:09 +0200

    Test downloadExercise, downloadOldSubmission, resetExercise

--- backend/setup.ts
+++ backend/setup.ts
@@ -1,3 +1,4 @@
+import archiver from "archiver";
 import fs from "fs";
 import { ncp } from "ncp";
 import fetch from "node-fetch";
@@ -8,6 +9,7 @@ import * as config from "../config";
 const LANGS_RUST_URL = config.productionApi.__TMC_LANGS_RUST_DL_URL__.replace(/"/g, "");
 const LANGS_RUST_VERSION = config.productionApi.__TMC_LANGS_RUST_VERSION__.replace(/"/g, "");
 
+// TODO: Make ../scr/utils/env.ts usable without webpack so that we get rid of duplicate code.
 const langsVersion = ((): string => {
     const platform = process.platform;
     const arch = process.arch;
@@ -26,25 +28,42 @@ const langsVersion = ((): string => {
             ? `tmc-langs-cli-x86_64-pc-windows-msvc-${LANGS_RUST_VERSION}.exe`
             : `tmc-langs-cli-i686-pc-windows-msvc-${LANGS_RUST_VERSION}.exe`;
     } else if (platform === "darwin") {
-        return arch === "x64"
-            ? "tmc-langs-cli-x86_64-apple-darwin-" + LANGS_RUST_VERSION
-            : "tmc-langs-cli-x86_64-unknown-linux-gnu-" + LANGS_RUST_VERSION;
+        if (arch === "arm64") {
+            return "tmc-langs-cli-aarch64-apple-darwin-" + LANGS_RUST_VERSION;
+        } else if (arch === "x64") {
+            return "tmc-langs-cli-x86_64-apple-darwin-" + LANGS_RUST_VERSION;
+        } else {
+            return "tmc-langs-cli-x86_64-unknown-linux-gnu-" + LANGS_RUST_VERSION;
+        }
     }
     return "other";
 })();
 
-const copyTMCPythonModules = (): void => {
+const copyTMCPythonModules = async (): Promise<void> => {
     const module = path.join(__dirname, "..", "submodules", "tmc-python-tester", "tmc");
     const courseDirectory = path.join(path.join(__dirname, "resources", "test-python-course"));
     const pythonExercises = fs
         .readdirSync(courseDirectory, { withFileTypes: true })
         .filter((x) => x.isDirectory())
-        .map((x) => path.join(courseDirectory, x.name, "tmc"));
+        .map((x) => path.join(courseDirectory, x.name));
     pythonExercises.forEach((exercise) => {
-        console.log(`Copying tmc module to ${exercise}`);
-        ncp(module, exercise, () => {});
+        const target = path.join(exercise, "tmc");
+        console.log(`Copying tmc module to ${target}`);
+        ncp(module, target, () => {});
     });
     console.log("Modules copied!");
+
+    await Promise.all(
+        pythonExercises.map(async (exercise) => {
+            console.log(`Creating download archive for ${exercise}`);
+            const archive = archiver("zip");
+            const archivePath = fs.createWriteStream(exercise + ".zip");
+            archive.pipe(archivePath);
+            archive.directory(exercise, false);
+            await archive.finalize();
+        }),
+    );
+    console.log("Archives created!");
 };
 
 const download = async (url: string, fileName: string): Promise<void> => {
@@ -70,7 +89,7 @@ const download = async (url: string, fileName: string): Promise<void> => {
 (async (): Promise<void> => {
     try {
         console.log("Copying tmc modules to python courses...");
-        copyTMCPythonModules();
+        await copyTMCPythonModules();
         console.log("Starting server setup...");
         await download(LANGS_RUST_URL + langsVersion, langsVersion);
         console.log("Setup complete!");
