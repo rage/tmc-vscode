2ce30b3a0bc783b3324d7b19f77203cd48a437e6
Jori Lampi <jori.lampi@helsinki.fi>
2020-12-18 12:16:19 +0200

    Bump TMC-langs from 0.6.9 to 0.8.0

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const path = require("path");
 
-const TMC_LANGS_RUST_VERSION = "0.6.9";
+const TMC_LANGS_RUST_VERSION = "0.8.0";
 
 const localTMCServer = {
     __TMC_LANGS_CONFIG_DIR__: JSON.stringify(null),
