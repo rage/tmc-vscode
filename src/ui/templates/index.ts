c3ec668b05dde20fa7e35aa0454f1c8d30a424c1
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-06-01 08:46:34 +0300

    Eslint

--- src/ui/templates/index.ts
+++ src/ui/templates/index.ts
@@ -1,8 +1,7 @@
 import * as CourseDetails from "./CourseDetails.jsx";
 import * as Login from "./Login.jsx";
 import * as MyCourses from "./MyCourses.jsx";
-import * as Settings from "./Settings.jsx";
 import * as Webview from "./Webview.jsx";
 import * as Welcome from "./Welcome.jsx";
 
-export { CourseDetails, Login, MyCourses, Settings, Webview, Welcome };
+export { CourseDetails, Login, MyCourses, Webview, Welcome };
