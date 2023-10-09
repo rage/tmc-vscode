3fb3212ac54dd69a0f02596638fdd7bd8cf97443
Heliozoa <daniel.x.martinez@helsinki.fi>
2023-10-09 12:37:37 +0300

    Download CLI to temp file and rename after finished

--- src/init/downloadCorrectLangsVersion.ts
+++ src/init/downloadCorrectLangsVersion.ts
@@ -28,13 +28,16 @@ async function downloadCorrectLangsVersion(
 
     deleteSync(cliFolder, { force: true });
 
+    // copy to temp file and rename to prevent partial downloads from causing issues
+    const tempPath = path.join(cliFolder, `temp-${executable}`);
+
     const cliUrl = TMC_LANGS_DL_URL + executable;
-    Logger.info(`Downloading TMC-langs from ${cliUrl} to ${cliPath}`);
+    Logger.info(`Downloading TMC-langs from ${cliUrl} to temporary file ${tempPath}`);
     const message = `Downloading TMC-langs ${TMC_LANGS_VERSION}...`;
     const langsDownloadResult = await dialog.progressNotification(
         message,
         async (progress) =>
-            await downloadFile(cliUrl, cliPath, undefined, (percent) => {
+            await downloadFile(cliUrl, tempPath, undefined, (percent) => {
                 // downloadFile gives both percent between 0-100 and discrete increment.
                 // Divide here at least until deciding if "increments" are no longer necessary.
                 progress.report({ message, percent: percent / 100 });
@@ -46,14 +49,22 @@ async function downloadCorrectLangsVersion(
     }
 
     try {
-        const fd = await fs.open(cliPath, "r+");
+        const fd = await fs.open(tempPath, "r+");
         await fs.fchmod(fd, 0o111);
         await fs.close(fd);
     } catch (e) {
-        Logger.error("Error changing permissions for CLI", e);
+        // not sure what the best way to handle errors here is
+        // this may be a good compromise between allowing expert users to take care of it themselves
+        // vs. allowing new users to easily just try again in case it was a fluke
+        Logger.error(
+            `Error changing permissions for CLI at ${tempPath}. \
+            You can try again or make it executable manually and remove the "temp-" prefix.`,
+            e,
+        );
         // Typing change from update
         return Err(e as Error);
     }
+    await fs.rename(tempPath, cliPath);
 
     return Ok(cliPath);
 }
