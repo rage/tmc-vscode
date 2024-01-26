0b68f434860e03832cbdfb51d96c5a933ce358d7
Heliozoa <daniel.x.martinez@helsinki.fi>
2024-01-26 13:41:35 +0200

    Default to release package in script

--- bin/package.bash
+++ bin/package.bash
@@ -3,9 +3,7 @@ set -euxo pipefail
 
 # requires @vscode/vsce
 # the script takes an argument that can be set to "--pre-release" to package as a pre-release
-# or to "" for a regular release
-# we could give this argument a default value, but requiring an explicit "" seems safer
-PRERELEASE_ARG=${1}
+PRERELEASE_ARG=${1:""}
 
 # clean old artifacts to ensure that local packages match the CI where they don't exist
 # otherwise, a package could work locally because some file happened to be there that's not there in CI
