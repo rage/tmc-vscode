611a8c0a774104cfb5c332861ec2d288f423b7f0
Heliozoa <daniel.x.martinez@helsinki.fi>
2024-01-26 13:39:16 +0200

    Improve package script

--- bin/package.bash
+++ bin/package.bash
@@ -1,15 +1,20 @@
 #!/bin/bash
+set -euxo pipefail
 
 # requires @vscode/vsce
-# the PRERELEASE env var can be set to "--pre-release" to package as a pre-release
+# the script takes an argument that can be set to "--pre-release" to package as a pre-release
+# or to "" for a regular release
+# we could give this argument a default value, but requiring an explicit "" seems safer
+PRERELEASE_ARG=${1}
 
 # clean old artifacts to ensure that local packages match the CI where they don't exist
 # otherwise, a package could work locally because some file happened to be there that's not there in CI
-rm ./webview-ui/public/build
+rm -rf ./dist
+rm -rf ./webview-ui/public/build
 
 # run ci and build webview
 npm run ci:all
 npm run webview:build
 
 # create package
-vsce package ${PRERELEASE}
+npx vsce package ${PRERELEASE_ARG}
