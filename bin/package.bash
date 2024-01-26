3aa52c6f58b5d20136b73325ec6a90545ee25cb3
Heliozoa <daniel.x.martinez@helsinki.fi>
2024-01-26 13:43:24 +0200

    Fix default value

--- bin/package.bash
+++ bin/package.bash
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # requires @vscode/vsce
 # the script takes an argument that can be set to "--pre-release" to package as a pre-release
-PRERELEASE_ARG=${1:""}
+PRERELEASE_ARG=${1:-""}
 
 # clean old artifacts to ensure that local packages match the CI where they don't exist
 # otherwise, a package could work locally because some file happened to be there that's not there in CI
