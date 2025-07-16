2e5cbf9988d62683f05be0b409a2e781f42fd219
Heliozoa <daniel.x.martinez@helsinki.fi>
2025-07-16 09:53:34 +0300

    Check langs shasum

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const path = require("path");
 
-const TMC_LANGS_RUST_VERSION = "0.37.1";
+const TMC_LANGS_RUST_VERSION = "0.38.0";
 
 const mockTmcLocalMooc = {
     __TMC_BACKEND_URL__: JSON.stringify("http://localhost:4001"),
--- src/init/downloadCorrectLangsVersion.ts
+++ src/init/downloadCorrectLangsVersion.ts
@@ -6,6 +6,7 @@ import { Err, Ok, Result } from "ts-results";
 import Dialog from "../api/dialog";
 import { TMC_LANGS_DL_URL, TMC_LANGS_VERSION } from "../config/constants";
 import { downloadFile, getLangsCLIForPlatform, getPlatform, Logger } from "../utilities";
+import { Sha256 } from "@aws-crypto/sha256-js";
 
 /**
  * Downloads correct langs version for the current extension version, unless already present. Will
@@ -21,50 +22,74 @@ async function downloadCorrectLangsVersion(
     const executable = getLangsCLIForPlatform(getPlatform(), TMC_LANGS_VERSION);
     Logger.info("TMC-Langs version: " + executable);
 
+    // download CLI if necessary
     const cliPath = path.join(cliFolder, executable);
-    if (fs.existsSync(cliPath)) {
-        return Ok(cliPath);
-    }
-
-    deleteSync(cliFolder, { force: true });
-
-    // copy to temp file and rename to prevent partial downloads from causing issues
-    const tempPath = path.join(cliFolder, `temp-${executable}`);
-
+    const shaPath = cliPath + ".sha256";
     const cliUrl = TMC_LANGS_DL_URL + executable;
-    Logger.info(`Downloading TMC-langs from ${cliUrl} to temporary file ${tempPath}`);
-    const message = `Downloading TMC-langs ${TMC_LANGS_VERSION}...`;
-    const langsDownloadResult = await dialog.progressNotification(
-        message,
-        async (progress) =>
-            await downloadFile(cliUrl, tempPath, undefined, (percent) => {
-                // downloadFile gives both percent between 0-100 and discrete increment.
-                // Divide here at least until deciding if "increments" are no longer necessary.
-                progress.report({ message, percent: percent / 100 });
-            }),
-    );
-    if (langsDownloadResult.err) {
-        Logger.error("An error occurred while downloading TMC-langs:", langsDownloadResult.val);
-        return langsDownloadResult;
+    const shaUrl = cliUrl + ".sha256";
+    if (!fs.existsSync(cliPath)) {
+        deleteSync(cliFolder, { force: true });
+
+        // copy to temp file and rename to prevent partial downloads from causing issues
+        const tempPath = path.join(cliFolder, `temp-${executable}`);
+
+        Logger.info(`Downloading TMC-langs from ${cliUrl} to temporary file ${tempPath}`);
+        const message = `Downloading TMC-langs ${TMC_LANGS_VERSION}...`;
+        const langsDownloadResult = await dialog.progressNotification(
+            message,
+            async (progress) =>
+                await downloadFile(cliUrl, tempPath, undefined, (percent) => {
+                    // downloadFile gives both percent between 0-100 and discrete increment.
+                    // Divide here at least until deciding if "increments" are no longer necessary.
+                    progress.report({ message, percent: percent / 100 });
+                }),
+        );
+        if (langsDownloadResult.err) {
+            Logger.error("An error occurred while downloading TMC-langs:", langsDownloadResult.val);
+            return langsDownloadResult;
+        }
+        try {
+            const fd = await fs.open(tempPath, "r+");
+            await fs.fchmod(fd, 0o755);
+            await fs.close(fd);
+        } catch (e) {
+            // not sure what the best way to handle errors here is
+            // this may be a good compromise between allowing expert users to take care of it themselves
+            // vs. allowing new users to easily just try again in case it was a fluke
+            Logger.error(
+                `Error changing permissions for CLI at ${tempPath}. \
+            You can try again or make it executable manually and remove the "temp-" prefix.`,
+                e,
+            );
+            // Typing change from update
+            return Err(e as Error);
+        }
+        await fs.rename(tempPath, cliPath);
     }
 
-    try {
-        const fd = await fs.open(tempPath, "r+");
-        await fs.fchmod(fd, 0o755);
-        await fs.close(fd);
-    } catch (e) {
-        // not sure what the best way to handle errors here is
-        // this may be a good compromise between allowing expert users to take care of it themselves
-        // vs. allowing new users to easily just try again in case it was a fluke
-        Logger.error(
-            `Error changing permissions for CLI at ${tempPath}. \
-            You can try again or make it executable manually and remove the "temp-" prefix.`,
-            e,
-        );
-        // Typing change from update
-        return Err(e as Error);
+    // download shasum if necessary
+    if (!fs.existsSync(shaPath)) {
+        const result = await downloadFile(shaUrl, shaPath);
+        if (result.err) {
+            Logger.error(
+                "An error occurred while downloading the checksum for TMC-langs:",
+                result.val,
+            );
+        }
     }
-    await fs.rename(tempPath, cliPath);
+
+    // check shasum
+    const cliHash = new Sha256();
+    const cliData = fs.readFileSync(cliPath);
+    cliHash.update(cliData);
+    const cliDigest = Buffer.from(cliHash.digestSync()).toString("hex");
+
+    const hashData = fs.readFileSync(shaPath, "utf-8").split(" ")[0];
+    if (cliDigest !== hashData) {
+        Logger.error("Mismatch between CLI and checksum, please try again");
+        deleteSync(cliFolder, { force: true });
+        return Err(new Error("Mismatch between CLI and checksum, please try again"));
+    }
 
     return Ok(cliPath);
 }
